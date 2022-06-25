7e404d8978f97a170f210d8953a50cd7fb005c81
Mateusz Tadla <mtadla@sii.pl>
2022-06-25 12:35:22 +0200

    add continents

--- src/test/java/FormTest.java
+++ src/test/java/FormTest.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -8,20 +9,30 @@ import java.util.List;
 public class FormTest extends TestBase{
 
     @Test
-    public void shouldFillFormWithSuccess(){
+    public void shouldFillFormWithSuccess() throws InterruptedException {
         driver.get("https://seleniumui.moderntester.pl/form.php");
 
         WebElement firstNameInput = driver.findElement(By.id("inputFirstName3"));
         firstNameInput.sendKeys("Jan");
 
+        driver.findElement(By.id("inputLastName3")).sendKeys("Kowalski");
         driver.findElement(By.id("inputAge3")).sendKeys("30");
+        driver.findElement(By.id("inputEmail3")).sendKeys("j.k@wp.pl");
 
-        List<WebElement> genders = driver.findElements(By.name("gridRadiosSex"));
-        genders.get(0).click();
+        driver.findElements(By.name("gridRadiosSex")).get(0).click();
+
+        List<WebElement> experience = driver.findElements(By.name("gridRadiosExperience"));
+        experience.get(3).click();
+
+        driver.findElement(By.id("gridCheckManulTester")).click();
 
         // tutaj uzupelniamy pozostale pola form
 
 
+        Select selectContinents = new Select(driver.findElement(By.id("selectContinents")));
+        selectContinents.selectByValue("europe");
+
+        // tutaj dodac obs selekcta 'selenium commands
 
 
 
