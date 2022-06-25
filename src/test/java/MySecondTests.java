239c9f02368ea2ff182ef1b9273ead1053e1a7b4
Mateusz Tadla <mtadla@sii.pl>
2022-06-25 10:31:27 +0200

    add test

--- src/test/java/MySecondTests.java
+++ src/test/java/MySecondTests.java
@@ -1,11 +1,33 @@
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class MySecondTests {
+    @BeforeMethod
+    public void setup(){
+        System.out.println("I will run before each test");
+    }
+
+    @AfterMethod
+    public void cleanUp(){
+        System.out.println("I will run AFTER each test");
+    }
+
+    @Test
+    public void shouldAddTwoNumbers(){
+        Assert.assertEquals(add(2,3), 5);
+    }
+
 
     public int add(int a, int b) {
         return a + b;
     }
 
-
     public int multiply(int a, int b) {
         return a * b;
     }
+
+
+
 }
